c60e0c78880a3a5352a579f69c3ea98c5ac3c68c
aimi-andertona <aimi.v.andertona@accenture.com>
2023-01-27 14:14:49 +0200

    Update imageProcessor.js

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const {Worker, isMainThread} = require('worker_threads');
-
 const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js');
 const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
 
